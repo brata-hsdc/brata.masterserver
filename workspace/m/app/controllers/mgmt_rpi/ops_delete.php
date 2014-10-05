c6e6b1d6f1c6a9f3ad8a6fe8bf81f05d4e96e127
joe daniels <joebackspace@gmail.com>
2014-10-05 13:49:44 -0400

    add rpi

---
+++ workspace/m/app/controllers/mgmt_rpi/ops_delete.php
@@ -0,0 +1,30 @@
+<?php
+function _ops_delete($OID=0,$CID=0) {
+  $OID=max(0,intval($OID));
+  $CID=max(0,intval($CID));
+  $msg='';
+  
+  loginRequireMgmt();
+  if (!loginCheckPermission(USER::MGMT_RPI)) redirect("errors/401");
+  $itemName="RPI";
+  $urlPrefix="mgmt_rpi";
+  $object=new Message($OID,$CID);
+  
+  if (!$object->exists())
+  $msg="$itemName not found!";
+  else {
+    transactionBegin();
+    if ($object->delete() )
+    {
+      transactionCommit();
+      $msg="$itemName deleted!";
+    }
+    else
+    {
+      TransactionRollback();
+      $msg="$itemName delete failed!";
+    }
+
+  }
+  redirect("$urlPrefix/manage",$msg);
+}
