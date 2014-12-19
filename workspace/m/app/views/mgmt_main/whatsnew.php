fb128f22a69bb3f027e6ea4d32d8b0fdcf7cbe10
dcarreira <joedaniels@gmail.com>
2014-12-19 12:34:32 -0500

    update todo and what's new

--- workspace/m/app/views/mgmt_main/whatsnew.php
+++ workspace/m/app/views/mgmt_main/whatsnew.php
@@ -1,19 +1,30 @@
 <h2>General Differences</h2>
-<p></p>
+<p>New requirement are forcing rewrite of almost of the server code</p>
 
 <h2>Database changes</h2>
+<p>redesign of station type objects in progress</p>
 
 <h2>Management Site</h2>
-<p>No Changes</p>
+<p>Schools CRUD working</p>
+<p>Team CRUD workding including generating random 5 digit pin for each team<p>
+
+<p>Adding CRUD for each station type</p>
 
 <h2>Public Site</h2>
+(all features depreciated due to new requirments)
 Leader Board 
 Score details
 
 <h2>rPI Interface</h2>
+<ul>
+<li>join
+<li>reset
+<li>shutdown
+</ul>
 <h2>Brata Interface</h2>
+<p>Register operation working</p>
 
-<p>
+<p>Brata testing mock interface.
 </p>
 
 
