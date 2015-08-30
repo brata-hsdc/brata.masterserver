3d72d964b7dca55b9f6968ce77e2c6df8ae7ff9b
Ellery Chan <ellery@precisionlightworks.com>
2015-08-30 13:05:00 -0400

    Start building database schema.
    Created a couple of models in dbkeeper/models.py and piservice/models.py.
    Added a schema diagram that reflects the models.

--- workspace/ms/dbkeeper/models.py
+++ workspace/ms/dbkeeper/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+# TODO:  Questions for Jaron about the 2015 design:
+#        1. Do we need the competitors and mentors to register with real
+#           user accounts?
+#        2. I plan to use Django's built-in authentication if auth is needed.
+#        3. What is the emailAddress ("email" in the old db) field used for?
+#        4. Do we need the fine-grained permissions that last year's db has?
+#           It looks like there were just 2 roles defined:  ADMIN (all perms)
+#           and NONE (no perms).
+
+#----------------------------------------------------------------------------
+# Adding profile information to the Django User class using what they call
+# a "profile model" that is linked to the User model with a one-to-one
+# relationship.  (See the explanation here:
+# https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending-the-existing-user-model)
+#
+# The default User class already contains username, password, email, first_name,
+# last_name.  If that's all we need, we don't have to do this at all.
+class Admin(models.Model):
+    user = models.OneToOneField(User)
+
+#----------------------------------------------------------------------------
+class School(models.Model):
+    class Meta:
+        managed = True  # We want manage.py to migrate database changes for us
+
+    # Constants
+    NAME_FIELD_LENGTH = 100
+    
+    # Schema definition
+    name = models.CharField(max_length=NAME_FIELD_LENGTH, unique=True)
+    
+#----------------------------------------------------------------------------
+class Team(models.Model):
+    class Meta:
+        managed = True  # We want manage.py to migrate database changes for us
+    
+    # Constants
+    NAME_FIELD_LENGTH = 100  # Make it long because some team will have a "creative" name
+    PIN_FIELD_LENGTH  = 20
+    
+    # Schema definition
+    name   = models.CharField(max_length=NAME_FIELD_LENGTH, unique=True)
+    school = models.ForeignKey(School)
+    pin    = models.CharField(max_length=PIN_FIELD_LENGTH, default="generated")  # TODO: what's this for? Do we need it or will the password be sufficient?
+    
+    #    Score fields for the different competitions
+    totalScore = models.IntegerField(default=0)
+    totalDuration = models.IntegerField(default=0)  # total duration of competition in seconds
+    # TODO:  Add more fields here as needed
+    
+#----------------------------------------------------------------------------
+class Mentor(models.Model):
+    """ Represents information about a team mentor, teacher, chaperone:  a
+        person of authority who represents the team and/or is a POC.
+    """
+    class Meta:
+        managed = True  # We want manage.py to migrate database changes for us
+
+    # Constants
+    NAME_FIELD_LENGTH  = 100
+    PHONE_FIELD_LENGTH = 20
+    NOTE_FIELD_LENGTH  = 2000
+    
+    # Schema definition
+    name        = models.CharField(max_length=NAME_FIELD_LENGTH, unique=True)
+    school      = models.ForeignKey(School)
+    workPhone   = models.CharField(max_length=PHONE_FIELD_LENGTH, blank=True)
+    mobilePhone = models.CharField(max_length=PHONE_FIELD_LENGTH, blank=True)
+    otherPhone  = models.CharField(max_length=PHONE_FIELD_LENGTH, blank=True)
+    note        = models.CharField(max_length=NOTE_FIELD_LENGTH, blank=True)
+    teams       = models.ManyToManyField(Team)
+    
--- workspace/ms/piservice/models.py
+++ workspace/ms/piservice/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from dbkeeper.models import Team
 
 # Create your models here.
+
+#----------------------------------------------------------------------------
+class PiStation(models.Model):
+    """ Holds information about the Raspberry Pi stations being used in the
+        competition.
+    """
+    class Meta:
+        managed = True  # We want manage.py to migrate database changes for us
+    
+    # Constants
+    NAME_FIELD_LENGTH = 100  # Make it long because some team will have a "creative" name
+    PIN_FIELD_LENGTH  = 20
+    
+    # Values for type
+    UNKNOWN_STATION_TYPE = 0
+    # TODO: add other types as appropriate
+    STATION_TYPE_CHOICES = (
+                             (UNKNOWN_STATION_TYPE, "Unknown"),
+                           )
+    
+    UNKNOWN_PI_TYPE = 0
+    A_PI_TYPE       = 1
+    B_PI_TYPE       = 2
+    BPLUS_PI_TYPE   = 3
+    # TODO: add other types as appropriate
+    PI_TYPE_CHOICES = (
+                        (UNKNOWN_PI_TYPE, "Unknown"),
+                        (A_PI_TYPE,       "Model A"),
+                        (B_PI_TYPE,       "Model B"),
+                        (BPLUS_PI_TYPE,   "Model B+"),
+                      )
+    
+    # Schema definition
+    hostname    = models.CharField()
+    ipAddress   = models.CharField()
+    stationType = models.PositiveSmallIntegerField(choices=STATION_TYPE_CHOICES, default=UNKNOWN_STATION_TYPE)
+    piType      = models.PositiveSmallIntegerField(choices=PI_TYPE_CHOICES, default=UNKNOWN_PI_TYPE)
+    
+#----------------------------------------------------------------------------
+class PiEvent(models.Model):
+    """ A time-stamped instance of a specific occurrence, what kind of
+        occurrence it was, who did it, and possibly a reference to some
+        other data.
+    """
+    # Constants
+    DATA_FIELD_LENGTH = 2000
+    # Values for type
+    UNKNOWN_TYPE = 0
+    # TODO: add other types as appropriate
+    TYPE_CHOICES = (
+                    (UNKNOWN_TYPE, "Unknown"),
+                   )
+    time = models.TimeField()
+    type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, default=UNKNOWN_TYPE)
+    teamID = models.ForeignKey(Team)
+    piID   = models.ForeignKey(PiStation)
+
+    # TODO: The data field could potentially be large if we start shoving
+    # big JSON objects into it.  Should it be part of this table or
+    # should it be a separate table that this one has a ForeignKey to?
+    # I think modern RDBMS's don't store the full field size for every
+    # record for char fields.  Am I wrong?
+    data = models.CharField(max_length=DATA_FIELD_LENGTH, blank=True)
+    
