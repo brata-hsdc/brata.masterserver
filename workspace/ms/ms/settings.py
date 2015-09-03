3c39dda65a7b3d8fec0813feb273e4eba2d371d7
Ellery Chan <ellery@precisionlightworks.com>
2015-09-02 20:15:45 -0400

    Deactivate CSRF in the Django project settings.py

--- workspace/ms/ms/settings.py
+++ workspace/ms/ms/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',  # turned off to make it easier to develop REST API
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
