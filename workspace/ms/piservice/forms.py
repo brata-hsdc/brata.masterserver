ea99390ba7df02aed2ed3a30f0f4c6e6bb6469f1
Ellery Chan <ellery.chan@gmail.com>
2016-02-19 00:11:44 -0500

    Fix formatting in piservice/forms.py

--- workspace/ms/piservice/forms.py
+++ workspace/ms/piservice/forms.py
@@ -8,6 +8,5 @@ from .models import PiStation
 #----------------------------------------------------------------------------
 class AddLibraryTestForm(forms.Form):
     stations = forms.ModelMultipleChoiceField(label="Station",
-                                      queryset=PiStation.objects.all().order_by("station_id"),
-#                                       widget=forms.MultipleSelectField(),
-                                      required=True)
+                                              queryset=PiStation.objects.all().order_by("station_id"),
+                                              required=True)
